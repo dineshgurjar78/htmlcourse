a1aafd746a0f72cddf6c536ec8215979d59e1c03
dinesh <chandeladinesh7849@gmail.com>
2024-07-25 16:25:52 +0530

    o1class

---
+++ 01class/index.js
@@ -0,0 +1,50 @@
+// var name = "hello"
+// console.log(name)
+
+// let name1 = "ham kamjor hai"
+// console.log(name1)
+
+// const name2 ="chora gurjar"
+// console.log(name2)
+
+// var num1 = 1000
+// console.log(num1)
+
+// let num2 = 2000
+// console.log(num2)
+
+
+
+
+
+// var num4 = 1000.55
+// console.log(num4)
+
+// let num5 = 2000.88
+// console.log(num5)
+
+// const num6 = 5000.12
+// console.log(num6)
+
+
+// let name = "hello"
+// console.log(typeof name )
+// console.log(name)
+
+// let name1 = 'dinesh gurjar'
+// console.log(typeof name1)
+// console.log(name1)
+
+
+// let num1 = 1000
+// console.log(typeof num1)
+// console.log(num1)
+
+// let num2 = 2000.52
+// console.log(typeof num2)
+// console.log(num2)
+
+
+let bool = true
+console.log(typeof bool)
+console.log(bool)
---
+++ 02class/index.js
@@ -0,0 +1,49 @@
+// console.log("Dinesh gurjar")
+// var name = "hello"
+// name = "world"
+// consol.log(name)
+
+// let name1 = "hello"
+// name1 = "world"
+// console.log(name1)
+
+// const name2 = "hello2"
+// name2 = "world2"
+// console.log(name2)
+
+// var name1 = "hello1"
+// var name1 = "world1"
+// console.log(name1)
+
+// let name2 = "hello2"
+// let name2 = "world2"
+// console.log(name2)
+
+// const name3 = "hello3"
+// const name3 = "world3"
+// console.log(name3)
+
+
+// var name1 = "hello1"
+// var name1 = "world1"
+
+
+// const obj = { "name":"dinesh","age":18,"islive":true}
+// console.log(obj)
+// console.log(obj["name"])
+// console.log(obj["age"])
+// console.log(obj["islive"])
+
+
+// const obj1 = {"first name":"dinesh","last name":"gurjar","address":"bansur"}
+// obj1["address"] = "jaipur"
+// console.log(obj1)
+
+const arr = ["dinesh",18,"bansur",true]
+arr[0] = "ram"
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+
+
